a41d582a5ad01ae5e770d11700946d034410266e
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-04-15 23:16:53 +0700

    Uncomment purchase module and enhance Purchase struct with updated account handling

--- programs/marketplace/src/contexts/purchase.rs
+++ programs/marketplace/src/contexts/purchase.rs
@@ -1,4 +1,13 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::Metadata,
+    token::{close_account, transfer_checked, CloseAccount, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
 use crate::errors::MarketplaceError;
 use crate::state::{Listing, Marketplace};
@@ -12,7 +21,7 @@ pub struct Purchase<'info> {
     #[account(mut)]
     pub taker: Signer<'info>,
     #[account(mut)]
-    pub maker: AccountInfo<'info>,
+    pub maker: SystemAccount<'info>,
     pub maker_mint: InterfaceAccount<'info, Mint>,
     #[account(
         seeds = [b"marketplace",marketplace.name.as_str().as_bytes()],
@@ -23,9 +32,17 @@ pub struct Purchase<'info> {
         init_if_needed,
         payer = taker,
         associated_token::mint = maker_mint,
-        associated_token::authority = maker,
+        associated_token::authority = taker,
     )]
     pub taker_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = rewards_mint,
+        associated_token::authority = taker,
+    )]
+    pub taker_rewards_ara: InterfaceAccount<'info, TokenAccount>,
     #[account(
         mut,
         associated_token::mint = maker_mint,
@@ -33,29 +50,31 @@ pub struct Purchase<'info> {
     )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
     #[account(
-    mut,
-    close = maker,
-seeds = [b"marketplace",key().as_ref(),maker_mint.key().as_ref()],
-bump = listing.bump,
-)]
+        mut,
+        close = maker,
+        seeds = [marketplace.key().as_ref(),maker_mint.key().as_ref()],
+        bump = listing.bump,
+    )]
     pub listing: Account<'info, Listing>,
     #[account(
-    mut,
-    seeds = [b"treasury",marketplace.key().as_ref()],
-    bump = marketplace.rewards_mint_bump,
-)]
+        mut,
+        seeds = [b"treasury",marketplace.key().as_ref()],
+        bump = marketplace.rewards_mint_bump,
+    )]
     pub treasury: SystemAccount<'info>,
     #[account(
-    seeds = [b"rewards",marketplace.key().as_ref()],
-    bump = marketplace.rewards_mint_bump,
-    mint::authority = marketplace,
-    mint::decimals = 6,
-)]
+        mut,
+        seeds = [b"rewards",marketplace.key().as_ref()],
+        bump = marketplace.rewards_mint_bump,
+        mint::authority = marketplace,
+        mint::decimals = 6,
+    )]
     pub rewards_mint: InterfaceAccount<'info, Mint>,
 
+    pub metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 impl<'info> Purchase<'info> {
@@ -70,15 +89,21 @@ impl<'info> Purchase<'info> {
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        let marketplace_fee = self
-            .listing
-            .price
-            .checked_mul(self.marketplace.fee as u64)
+        // let marketplace_fee = self
+        //     .listing
+        //     .price
+        //     .checked_mul(self.marketplace.fee as u64)
+        //     .ok_or(MarketplaceError::ArithmeticOverflow)?
+        //     .checked_div(1000_u64)
+        //     .ok_or(MarketplaceError::ArithmeticOverflow)?;
+
+        let marketplace_fee = (self.marketplace.fee as u64)
+            .checked_mul(self.listing.price)
             .ok_or(MarketplaceError::ArithmeticOverflow)?
-            .checked_div(1000_u64)
+            .checked_div(10000_u64)
             .ok_or(MarketplaceError::ArithmeticOverflow)?;
 
-        let amount = self.listing.price - marketplace_fee;
+        let amount = self.listing.price.checked_sub(marketplace_fee).unwrap();
 
         transfer(cpi_ctx, amount)?;
 
@@ -91,9 +116,7 @@ impl<'info> Purchase<'info> {
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        transfer(cpi_ctx, marketplace_fee)?;
-
-        Ok(())
+        transfer(cpi_ctx, marketplace_fee)
     }
     // transfer the nft
     pub fn transfer_nft(&mut self) -> Result<()> {
@@ -116,24 +139,27 @@ impl<'info> Purchase<'info> {
 
         let cpi_ctx = CpiContext::new_with_signer(programs, accounts, signer_seeds);
 
-        transfer_checked(cpi_ctx, self.maker_mint.decimals)?;
-        Ok(())
+        transfer_checked(cpi_ctx, 0, self.maker_mint.decimals)
     }
     // close the account
     pub fn close_vault_account(&mut self) -> Result<()> {
         let seeds = &[
-            &b"marketplace"[..],
+            // &b"marketplace"[..],
             &self.marketplace.key().to_bytes()[..],
             &self.maker_mint.key().to_bytes()[..],
             &[self.listing.bump],
         ];
         let signer_seeds = &[&seeds[..]];
 
-        let accounts = CloseAccount {
+        let cpi_program = self.token_program.to_account_info();
+
+        let close_accounts = CloseAccount {
             account: self.vault.to_account_info(),
-            destination: self.treasury.to_account_info(),
-            authority: self.maker.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.listing.to_account_info(),
         };
-        Ok(())
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, close_accounts, signer_seeds);
+        close_account(cpi_ctx)
     }
 }
